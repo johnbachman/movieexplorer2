8e4ce30a95b6932f013d97f170b03205381cde99
John Bachman <bachmanjohn@gmail.com>
2012-11-21 13:23:04 -0500

    Got Roi serialization to work correctly!
    The problem was that direct serialization of Rois would throw
    NotSerializable exceptions from sun.java2d.graphics2d. As a workaround,
    I wrote custom writeObject and readObject methods to deal with Roi
    serialization in the Keyframe class.

--- src/Keyframe.java
+++ src/Keyframe.java
@@ -1,18 +1,22 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import ij.gui.Roi;
 import ij.gui.PolygonRoi;
+import ij.io.RoiDecoder;
+import ij.io.RoiEncoder;
+
 import java.awt.Rectangle;
 
 public class Keyframe implements Comparable<Keyframe>, Serializable {
 
 	static final long serialVersionUID = 3L;
 	
-	private Roi roi;
 	private int frame;
 	private boolean momp;
 	private boolean last;
+	private transient Roi roi; // See comments below for serialization methods
 	
-
 	/**
 	 * Constructor. Sets the frame and roi with the given parameters, and sets
 	 * the MOMP and Last flags to false.
@@ -39,6 +43,84 @@ public class Keyframe implements Comparable<Keyframe>, Serializable {
 		this.last = last;
 	}
  	
+	/**
+	 * Implementation of custom writeObject method for serialization. This is
+	 * necessary because direct serialization of the Roi object results in a
+	 * java.io.NotSerializableException for sun.java2d.SunGraphics2D (which
+	 * must be used somewhere by the geometry objects used by the ImageJ
+	 * Roi class).
+	 *
+	 * As a workaround, this method uses the ImageJ RoiEncoder class to convert
+	 * the Roi object to a byte array, and writes the number of bytes to the
+	 * ObjectOutputStream before writing the bytes themselves. This way the
+	 * number of bytes to read can be determined by reading the int, then
+	 * a byte array of the appropriate size can be created, the Roi bytes can
+	 * be read, and then finally decoded back to an Roi using the RoiDecoder
+	 * class.
+	 *
+	 * @param oos
+	 */
+	private void writeObject(ObjectOutputStream oos) {
+		try {
+			// Write the integer and boolean fields
+			Movie_Explorer_2.log("About to call Keyframe writeobject for Keyframe");
+			oos.writeInt(frame);
+			oos.writeBoolean(momp);
+			oos.writeBoolean(last);
+			Movie_Explorer_2.log("Wrote int, bool, bool, about to call write for ROI");
+
+			// Write the ROI as a byte array
+			byte[] roiBytes = RoiEncoder.saveAsByteArray(roi);
+			Movie_Explorer_2.log(Integer.toString(roiBytes.length) + " bytes to write");
+			oos.writeInt(roiBytes.length);
+			oos.write(roiBytes);
+			Movie_Explorer_2.log("ROI successfully written");
+		}
+		catch (Exception e) {
+			Movie_Explorer_2.log("Kf writeobject exception: " + e.toString());
+		}
+	}
+
+	/**
+	 * Implementation of custom readObject method for serialization. This is
+	 * necessary because direct serialization of the Roi object results in a
+	 * java.io.NotSerializableException for sun.java2d.SunGraphics2D (which
+	 * must be used somewhere by the geometry objects used by the ImageJ
+	 * Roi class).
+	 *
+	 * As a workaround, this method uses the ImageJ RoiEncoder class to convert
+	 * the Roi object to a byte array, and writes the number of bytes to the
+	 * ObjectOutputStream before writing the bytes themselves. This way the
+	 * number of bytes to read can be determined by reading the int, then
+	 * a byte array of the appropriate size can be created, the Roi bytes can
+	 * be read, and then finally decoded back to an Roi using the RoiDecoder
+	 * class.
+	 *
+	 * @param ois
+	 */
+	private void readObject(ObjectInputStream ois) {
+		try {
+			// Read the integer and boolean fields
+			Movie_Explorer_2.log("About to call Keyframe readObject for Keyframe");
+			this.frame = ois.readInt();
+			this.momp = ois.readBoolean();
+			this.last = ois.readBoolean();
+			Movie_Explorer_2.log("Read int, bool, bool, about to call read for ROI");
+
+			// Read the Roi as a byte array
+			int numBytes = ois.readInt();
+			Movie_Explorer_2.log(Integer.toString(numBytes) + " bytes to read");
+			byte[] roiBytes = new byte[numBytes];
+			int numBytesRead = ois.read(roiBytes);
+			Movie_Explorer_2.log(Integer.toString(numBytesRead) + " bytes read");			
+			this.roi = RoiDecoder.openFromByteArray(roiBytes);
+			Movie_Explorer_2.log("ROI successfully read");
+		}
+		catch (Exception e) {
+			Movie_Explorer_2.log("Kf writeobject exception: " + e.toString());
+		}
+	}
+
 	/**
 	 * Implementation of the Comparable interface, allows the Keyframes to
 	 * be sorted in frame order.
